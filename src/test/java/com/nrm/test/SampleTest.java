2c11423f38dc91bcb4e0aade1a42f317170e8efc
njsingh15 <njsingh15@gmail.com>
2017-11-05 10:12:23 +0530

    added new test line of code

--- src/test/java/com/nrm/test/SampleTest.java
+++ src/test/java/com/nrm/test/SampleTest.java
@@ -5,6 +5,7 @@ public class SampleTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("Welcome to Test case scenario 1");
+       System.out.println("Added new line of code in tests");
 	}
 
 }
