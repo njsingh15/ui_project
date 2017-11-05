53164b4d3327103cff0beaa2eb24147ac7f4438c
manee2k6 <manee2k6@gmail.com>
2017-11-05 09:56:06 +0530

    added source and gitignore file

---
+++ src/main/java/com/nrm/practise/FirstJavaProgram.java
@@ -0,0 +1,12 @@
+package com.nrm.practise;
+
+public class FirstJavaProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to DevOps Training");
+
+	}
+
+}
---
+++ src/test/java/com/nrm/test/SampleTest.java
@@ -0,0 +1,10 @@
+package com.nrm.test;
+
+public class SampleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Welcome to Test case scenario 1");
+	}
+
+}
